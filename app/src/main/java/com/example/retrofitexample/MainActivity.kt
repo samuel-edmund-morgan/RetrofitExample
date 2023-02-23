603850b4d39c15fc5e580c357dbe040f8f5f8966
samuel-edmund-morgan <samuel.edmund.morgan@gmail.com>
2023-02-23 15:38:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/retrofitexample/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.retrofitexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retrofitexample.databinding.ActivityMainBinding
+import com.example.retrofitexample.retrofit.ProductAPI
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class MainActivity : AppCompatActivity() {
+    private val activityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+
+
+        //First we neeed to create Retrofit instance:
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        //Next we need to create API instance:
+        val productApi = retrofit.create(ProductAPI::class.java)
+
+
+        //Let's run our GET request in coroutine by pressing button (we SHOULD NOT use
+        // network requests in main thread
+        activityMainBinding.button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+            val product = productApi.getProductById(1)
+                withContext(Dispatchers.Main){
+                    activityMainBinding.textView.text = product.title
+                }
+            }
+        }
+
+
+    }
+
+
+}
