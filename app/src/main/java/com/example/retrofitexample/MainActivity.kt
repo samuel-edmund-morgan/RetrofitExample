8110a0c389105fe27522503c8785d43928770d68
samuel-edmund-morgan <samuel.edmund.morgan@gmail.com>
2023-02-23 16:00:26 +0200

    Added Interceptor and OkHttp client

--- app/src/main/java/com/example/retrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/retrofitexample/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.example.retrofitexample.databinding.ActivityMainBinding
 import com.example.retrofitexample.retrofit.ProductAPI
 import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -15,15 +17,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
 
+        //If we want (we WANT for debugging) to monitor which requests were sent, let's
+        //create interceptor and OkHttp client:
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
 
-        //First we need to init Retrofit instance:
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+
+
+        //First we need to init Retrofit instance (with client for debugging in Logcat):
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com")
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         //Next we need to init API instance:
         val productApi = retrofit.create(ProductAPI::class.java)
+
+
+
 
 
         //Let's run our GET request in coroutine by pressing button (we SHOULD NOT use
